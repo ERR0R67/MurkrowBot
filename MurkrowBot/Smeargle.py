958f6e29a076bd8b9dd4c3385d3813e6d094334b
ERR0R67 <gmans0394@gmail.com>
2025-09-03 22:21:20 -0500

    Update Smeargle.py

--- MurkrowBot/Smeargle.py
+++ MurkrowBot/Smeargle.py
@@ -19,17 +19,18 @@ def sketch(filepath, hotkey='ctrl+alt+z', delay = 2.5):
         print(f'File not found: {filepath}')
         return
     
+    # Split non-unique item names into a separate list of commands to enter manually
     for line in lines:
         if "item:" in line:
-            item = line.split("item:")[-1].strip()
-            if item in keywords:
+            item = line.split("item:")[-1].strip()  # Split each line at the item to see what item each command is
+            if item in keywords:                    # Place non-unique items in a different list
                 excluded_lines.append(line)
             else:
-                valid_lines.append(line)
+                valid_lines.append(line)            # All other lines get added to the automated section
         else:
             valid_lines.append(line)
 
-    duration = round(len(valid_lines) * delay / 60)
+    duration = round(len(valid_lines) * delay / 60) # For making time estimate
     print(f'File loaded, estimated completion in {duration} minutes')
     print(f'Place cursor in discord chat and press "{hotkey.upper()}" to start')
 
@@ -43,15 +44,16 @@ def sketch(filepath, hotkey='ctrl+alt+z', delay = 2.5):
         pyautogui.press('enter')        # Press Enter
         time.sleep(delay)               # Delay so bot can buffer
 
+    # Entry for non-unique item names, manually entered
     linecount = len(excluded_lines)
     print(f'Initial lines finished, starting manual input of {linecount} lines')
     print(f'Please enter {hotkey.upper()} to continue')
     keyboard.wait(hotkey)    
     
     for line in excluded_lines:
-        pyperclip.copy(line)            # Copy line from text file
-        print('paste line to proceed to next')
-        keyboard.wait('ctrl+v')              
+        pyperclip.copy(line)                    # Copy line from text file
+        print('paste line to proceed to next')  
+        keyboard.wait('ctrl+v')                 # Pressing paste pastes the current line and then advances to the next one automatically
         time.sleep(delay)
 
     print('Completed')
